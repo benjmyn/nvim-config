60bf83603f7c920f4adf901af34e38d80d7af09e
benjmyn <benjmyn@vt.edu>
2026-02-09 11:17:07 -0500

    First init.lua file

---
+++ init.lua
@@ -0,0 +1,14 @@
+-- Change leader to spacebar -- 
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Line numbers --
+vim.opt.number = true -- Show line numbers
+vim.opt.relativenumber = true -- Show relative line numbers
+vim.opt.numberwidth = 4 -- Line width (default 4) 
+
+-- Basic neovim config --
+vim.opt.mouse = '' -- Disable mouse
+vim.opt.tabstop = 4 -- Tab width
+vim.opt.cursorline = false -- Show line cursor is on
+vim.opt.cursorcolumn = false -- Show column cursor is on
