8abf79e91a5c9e78214a3605d9f8f325326bb79d
benjmyn <benjmyn@vt.edu>
2026-02-10 18:07:21 -0500

    Set up lazy with Neotree

---
+++ lua/config/settings.lua
@@ -0,0 +1,24 @@
+-- General config --
+vim.opt.mouse = '' 
+
+-- Format options (fo-table) --
+vim.opt.formatoptions = 'j,c,,,q,l'
+
+-- Set font type --
+vim.g.have_nerd_font = true 
+
+-- Line numbers --
+vim.opt.number = true 
+vim.opt.relativenumber = true 
+vim.opt.numberwidth = 4 
+
+-- Tab characteristics --
+vim.opt.tabstop = 4 
+vim.opt.shiftwidth = 4 
+
+-- Lowercase searching --
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Splitting settings (:new) --
+
